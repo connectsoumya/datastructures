a11ff57c11eccd8c7297bf32a0ee295057cd9650
soumya <connectsoumya@gmail.com>
2020-08-06 00:20:57 +0100

    single and double linked list completed

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -9,7 +9,8 @@ class DoublyLinkedList(object):
     def __init__(self):
         self.head = Node()
 
-    def insert(self, node_data):
+    def append(self, node_data):
+        node_data = Node(node_data)
         if self.head.data is None:
             self.head = node_data
         else:
@@ -18,6 +19,12 @@ class DoublyLinkedList(object):
                 current_node = current_node.next
             current_node.next = node_data
             current_node.next.previous = current_node
+
+    def prepend(self, node_data):
+        node_data = Node(node_data)
+        node_data.next = self.head
+        self.head.previous = node_data
+        self.head = node_data
 
     def delete(self, node_data):
         current_node = self.head
@@ -30,12 +37,20 @@ class DoublyLinkedList(object):
         current_node.next = None
         current_node.previous = None
 
+    def search(self, node_data):
+        current_node = self.head
+        while current_node.data != node_data:
+            previous_node = current_node
+            current_node = current_node.next
+        next_node = current_node.next
+        return previous_node, next_node
 
 
 if __name__ == '__main__':
     l = DoublyLinkedList()
     for i in range(5):
-        node_data = Node(i)
-        l.insert(node_data)
+        l.append(i)
+    for i in range(3):
+        l.prepend(i)
     l.delete(3)
     a = 0
--- linkedlist.py
+++ linkedlist.py
@@ -8,7 +8,8 @@ class LinkedList(object):
     def __init__(self):
         self.head = Node()
 
-    def insert(self, node_data):
+    def append(self, node_data):
+        node_data = Node(node_data)
         if self.head.data is None:
             self.head = node_data
         else:
@@ -16,6 +17,11 @@ class LinkedList(object):
             while current_node.next is not None:
                 current_node = current_node.next
             current_node.next = node_data
+
+    def prepend(self, node_data):
+        node_data = Node(node_data)
+        node_data.next = self.head
+        self.head = node_data
 
     def delete(self, node_data):
         current_node = self.head
@@ -31,7 +37,8 @@ class LinkedList(object):
 if __name__ == '__main__':
     l = LinkedList()
     for i in range(5):
-        node_data = Node(i)
-        l.insert(node_data)
+        l.append(i)
+    for i in range(2):
+        l.prepend(i)
     l.delete(3)
     a = 0
